e291503ccb042f0c029cd3206f4ac047193b943d
cranatic101 <suhaasvellanki@hotmail.com>
2024-02-23 17:06:45 -0800

    added recording code

--- audio_processing.py
+++ audio_processing.py
@@ -1,7 +1,49 @@
 from google.cloud import speech
 import text2emotion as te
+from speech_recognition import AudioSource, AudioData
+import speech_recognition as sr
+from time import sleep
 import os
+import keyboard
+from datetime import datetime
+import time
 import io
+def test():
+    go = 1
+
+    def quit():
+        
+        print("exiting...")
+        nonlocal go
+        go = 0
+
+    keyboard.on_press_key("q", lambda _:quit()) # press q to quit
+
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    #print(sr.Microphone.list_microphone_names())
+
+    mic = sr.Microphone(device_index=1)
+
+    print('listening...')
+    while go:
+        try:
+            sleep(0.01)
+            with mic as source:
+                audio = r.listen(source)
+                #print('go: ',go)
+                #print(r.recognize_google(audio))
+        except Exception as e:
+            print(e)
+            pass
+    
+    name = 'log_'+datetime.now().strftime("%m-%d_%H-%M")
+    path = 'recordings/'+name+'.wav'
+    with open(path,"wb") as file:
+        file.write(audio.get_wav_data())
+    print('uploading to '+path)
+    return path
+
 
 def wav_to_text(file_name):
 #setting Google credential
@@ -35,4 +77,6 @@ def emotion_detection(text):
 
 
 
-print(emotion_detection(wav_to_text("test2.wav")))
+#print(emotion_detection(wav_to_text("recordings/test2.wav")))
+#print(record_audio())
+test()
